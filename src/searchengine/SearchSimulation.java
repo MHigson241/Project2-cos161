961f3f33d49e9d13399f8cffb7efc3660965da3c
Retr0-Girlie <iris.mccain@maine.edu>
2026-04-10 13:22:07 -0400

    added the eval.pdf and queries to searchsim
    Eval pdf added which I will also share with Maddy for submission. Made some edits to searchsimulation.

--- src/searchengine/SearchSimulation.java
+++ src/searchengine/SearchSimulation.java
@@ -7,28 +7,27 @@ public class SearchSimulation {
 	public static long start;
 	public static long end;
 	public static void main(String[] args) {
-		ArrayList<Song> s = SingerAnalyzer.loadFile("C:\\Users\\dhigs\\Desktop\\Eclipse Workspace\\Project2-cos161\\src\\searchengine\\song_lyrics.tsv");
+		ArrayList<Song> s = SingerAnalyzer.loadFile("C:\\Users\\Eris Nyx\\Downloads\\song_lyrics (1).tsv");
 		//("C:\\Users\\Iris\\Downloads\\song_lyrics.tsv")   - Saving my file path for testing
 		//("C:\\Users\\Eris Nyx\\Downloads\\song_lyrics (1).tsv") saving more file paths
-		System.out.println("file loaded! :3");
 		SingerAnalyzer sing = new SingerAnalyzer(s);
 		ArrayList<String> queeries = new ArrayList<String>();
-		queeries.add("a");
-		queeries.add("b");
-		queeries.add("c");
-		queeries.add("g");
-		queeries.add("h");
-		queeries.add("j");
-		queeries.add("o");
-		queeries.add("k");
-		queeries.add("l");
-		queeries.add("p");
+		queeries.add("heartbreak");
+		queeries.add("fever");
+		queeries.add("money");
+		queeries.add("chain");
+		queeries.add("pizza");
+		queeries.add("rock of ages");
+		queeries.add("set fire");
+		queeries.add("right hand");
+		queeries.add("book");
+		queeries.add("limelight");
 		for (String q :queeries) {
-			long start = System.nanoTime();
+			double start = System.nanoTime();
 			Map<String, Integer> a = SingerAnalyzer.getTermFrequency(q);
 			sing.getIDFTry2();
-			long end = System.nanoTime();
-			long time = (end - start);
+			double end = System.nanoTime();
+			double time = (end - start)/1000000000;
 			System.out.print(time + a.toString());
 			System.out.println(sing.search(q, 5));
 		}
